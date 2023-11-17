16824f29a706e5e0c70e633d3cccbf599d7745bc
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-11-17 18:11:29 +0800

    asda

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
 
       <div className='col-lg-7 col-md-6 col-12 row d-flex p-lg-5 p-md-4 p-sm-3 p-4 justify-content-start'>
             
-        <div className=''>
+        <div className='contact'>
         <img src="email.png" alt="" style={{objectFit: 'cover', width: '50px', height: 'auto'}}  />
         <p className='mt-1 p-0 m-0 description' style={{fontWeight: 'bold', fontSize: '16px', color: '#323131'}} > 
             Email
