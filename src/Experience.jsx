de37a64110b549905f37d5b0a8b2a2bb458a0aee
jenobellido <jenodorobellido16@gmail.com>
2025-04-11 23:24:34 +0800

    Update Experience component: added SharpNexus role, adjusted order of experience items, and included logo image.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import snLogo from './sn-logo.svg'
 
 const Experience = () => {
   return (
@@ -12,7 +13,7 @@ const Experience = () => {
             </div>
 
             <div className='d-flex col-12 row justify-content-start p-lg-4 p-4' >
-                <div className='col-lg-4 col-md-6 col-sm-6 col-12 d-lg-flex d-flex pb-5 order-3'>
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 d-lg-flex d-flex pb-5 order-4'>
                     <div className=' rounded-5'>
                         <div className='project-bg'>
                             <div className='mb-3'>
@@ -20,13 +21,13 @@ const Experience = () => {
                             </div>
                             <div>
                                 <h4 className='mb-1'> Software Developer </h4> 
-                                <p style={{ textAlign: 'justify', fontWeight: '300'}} > Spring Valley Tech Corp · Internship <br /> Feb 2024 - Present <br /> Negros Occidental, Western Visayas, Philippines · On-site </p>
+                                <p style={{ textAlign: 'justify', fontWeight: '300'}} > Spring Valley Tech Corp · Internship <br /> Negros Occidental, Western Visayas, Philippines · On-site </p>
                                 <p style={{ textAlign: 'justify', fontWeight: '300'}} > • Engaged in an immersive <b> software development training </b> program while also gaining <b>experience in cloud-based development, specifically, AWS.</b></p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='col-lg-4 col-md-6 col-sm-6 col-12 d-lg-flex d-flex pb-5 order-2'>
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 d-lg-flex d-flex pb-5 order-3'>
                 <div className=' rounded-5'>
                         <div className='project-bg'>
                             <div className='mb-3'>
@@ -34,7 +35,7 @@ const Experience = () => {
                             </div>
                             <div>
                                 <h4 className='mb-1'> Software Engineer </h4> 
-                                <p style={{ textAlign: 'justify' , fontWeight: '300'}}> Forever Passion Dream Inc. · Part-time <br /> Feb 2024 - Apr 2024 <br /> New York, United States · Remote </p>
+                                <p style={{ textAlign: 'justify' , fontWeight: '300'}}> Forever Passion Dream Inc. · Part-time <br /> New York, United States · Remote </p>
                                 <p style={{ textAlign: 'justify', fontWeight: '300'}}> 
                                 • Worked as a <b>software engineer</b> specializing in developing web applications that <b> leverage
                                 artificial intelligence, and e-commerce development (Shopify) </b> to address the specific
@@ -44,7 +45,7 @@ const Experience = () => {
                         </div>                        
                     </div>
                 </div>
-                <div className='col-lg-4 col-md-6 col-sm-6 col-12 d-lg-flex d-flex pb-5 order-1'>
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 d-lg-flex d-flex pb-5 order-2'>
                 <div className=' rounded-5'>
                         <div className='project-bg'>
                             <div className='mb-3'>
@@ -52,7 +53,7 @@ const Experience = () => {
                             </div>
                             <div>
                                 <h4 className='mb-1'> Software Engineer </h4> 
-                                <p style={{ textAlign: 'justify', fontWeight: '300'}} > Mediaset · Full-time <br /> Apr 2024 - Present <br /> Oslo, Norway · Remote </p>
+                                <p style={{ textAlign: 'justify', fontWeight: '300'}} > Mediaset · Full-time <br />  Oslo, Norway · Remote </p>
                                 <p style={{ textAlign: 'justify', fontWeight: '300'}} > 
                                 • Responsible for <b>full-stack development, planning and designing concepts</b>, and implementing them into a working product.
                                 </p>
@@ -60,6 +61,26 @@ const Experience = () => {
                         </div>                        
                     </div>
                 </div>
+
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 d-lg-flex d-flex pb-5 order-1'>
+                    <div className=' rounded-5'>
+                        <div className='project-bg'>
+                            <div className='mb-3'>
+                                <img src={snLogo} style={{width: '50%'}} alt="" />
+                            </div>
+                            <div>
+                                <h4 className='mb-1'> AI Software Engineer </h4> 
+                                <p style={{ textAlign: 'justify', fontWeight: '300'}} > SharpNexus · Full-time <br /> Toronto, Canada · Remote </p>
+                                <p style={{ textAlign: 'justify', fontWeight: '300'}} > 
+                                • Responsible for full-stack development, including planning, designing, and implementing features across the entire software development lifecycle, along with integration of cloud solutions (AWS)
+                                <br /> • Led the implementation of a Retrieval-Augmented Generation (RAG) system using OpenAI and open-source models, featuring real-time, voice-based interaction that allows users to talk naturally with the AI—creating a fluid, human-like experience for discussing database data. 
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
         </div>
